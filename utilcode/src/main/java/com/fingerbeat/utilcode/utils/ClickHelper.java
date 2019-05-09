8071eb88074bae51cc21cd7dd86ab319e48ce462
xiecaibao <825302814@qq.com>
2019-05-09 10:16:04 +0800

    1

---
+++ utilcode/src/main/java/com/fingerbeat/utilcode/utils/ClickHelper.java
@@ -0,0 +1,20 @@
+package com.fingerbeat.utilcode.utils;
+
+/**
+ * Created by xiecaibao on 2018/12/27
+ */
+public class ClickHelper {
+    // 两次点击按钮之间的点击间隔不能少于1000毫秒
+    private static final int MIN_CLICK_DELAY_TIME = 1000;
+    private static long lastClickTime;
+    // 重复点击
+    public static boolean isRealClick() {
+        boolean flag = false;
+        long curClickTime = System.currentTimeMillis();
+        if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
+            flag = true;
+        }
+        lastClickTime = curClickTime;
+        return flag;
+    }
+}
